b16789a02c4bd43cf9d19946e0d6eaec8d3ccf4b
AdamMc331 <amcneilly331@gmail.com>
2016-09-29 15:07:05 -0400

    Added about page library.

--- app/src/main/java/com/androidessence/cashcaretaker/activities/AboutActivity.java
+++ app/src/main/java/com/androidessence/cashcaretaker/activities/AboutActivity.java
@@ -1,11 +1,15 @@
 package com.androidessence.cashcaretaker.activities;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.androidessence.cashcaretaker.BuildConfig;
 import com.androidessence.cashcaretaker.R;
 import com.androidessence.cashcaretaker.core.CoreActivity;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class AboutActivity extends CoreActivity {
 
@@ -16,8 +20,21 @@ public class AboutActivity extends CoreActivity {
 
         setupToolbar(true);
 
-        // Setup version
-        TextView versionTextView = (TextView) findViewById(R.id.app_version);
-        versionTextView.setText(String.format(getString(R.string.version_string), BuildConfig.VERSION_NAME));
+        Element versionElement = new Element();
+        versionElement.setTitle("Version: " + BuildConfig.VERSION_NAME);
+
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.drawable.cashcaretaker)
+                .setDescription("Cash Caretaker is a finance tracker that will keep track of all of your bank accounts and credit cards in one place. You will no longer have to deal with multiple apps to keep track of your accounts at the bank, and each of your different credit cards. With this personal checkbook you will be able to record all of your financial information to get a quick overview of all of your money at once.")
+                .addItem(versionElement)
+                .addGroup("Connect")
+                .addWebsite("http://androidessence.com")
+                .addPlayStore("com.androidessence.cashcaretaker")
+                .addGitHub("adammc331/cashcaretaker")
+                .create();
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.root);
+        rootView.addView(aboutPage);
     }
 }
